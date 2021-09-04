4ac6ce5dffd26afbf4129b9beb756c777a9683a9
Salim66 <salimhasanriad8@gmail.com>
2021-09-04 20:14:08 +0600

    Design product details page

--- resources/views/front/product_details.blade.php
+++ resources/views/front/product_details.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
 
-<?php echo $product; ?>
+<div style="width: 20rem; margin: 100px auto 0;">
+    <div>
+        <img src="{{ URL::to('/') }}/images/{{ $product->image }}" alt="product image">
+
+        <div>
+            <h4>Cart Title</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, iste!</p>
+            <a href="#">Read More</a>
+        </div>
+    </div>
+    <div class="product-information">
+        <img src="" alt="">
+        <div class="product-information">
+            <img src="" alt="">
+            <p><?php echo ucwords($product->pro_name); ?></p>
+        </div>
+    </div>
+</div>
 
 @endsection
