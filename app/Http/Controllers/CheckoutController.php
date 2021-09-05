4142923ba953091e640b4e51fc9f93e28f10ec30
Salim66 <salimhasanriad8@gmail.com>
2021-09-05 07:53:08 +0600

    Add validation to checkout controller and store

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,28 +15,43 @@ class CheckoutController extends Controller
         if(Auth::check()){
             return view('front.checkout');
         }else {
-            echo "Not Login";
+            return redirect()->route('login');
         }
-        return redirect('/products');
     }
 
     /**
      * Store Checkout page
      */
     public function formvalidate(Request $request){
+        // $this->validate($request, [
+        //     'fullname'  => 'required|min:5|max:35',
+        //     'username'  => 'required|min:5|max:30',
+        //     'password'  => 'required|min:5|max:25',
+        //     'cpassword' => 'required|min:5|max:30|name:password'
+        // ],
+        // [
+        //     'fullname.required'     =>  "Enter full name",
+        //     'password.required'     =>  "Please provide password",
+        //     'cpassword.required'    =>  "Password does not match"
+        // ]);
+
         $this->validate($request, [
-            'fullname'  => 'required|min:5|max:35',
-            'username'  => 'required|min:5|max:30',
-            'password'  => 'required|min:5|max:25',
-            'cpassword' => 'required|min:5|max:30|name:password'
-        ],
-        [
-            'fullname.required'     =>  "Enter full name",
-            'password.required'     =>  "Please provide password",
-            'cpassword.required'    =>  "Password does not match"
+            'fullname'  => 'required|min:5|max:30',
+            'state'     => 'required|min:2|max:25',
+            'pincode'   => 'required|numeric',
+            'city'      => 'required|min:5|max:30',
+            'country'   => 'required|min:5|max:30'
         ]);
 
-
+        Address::create([
+            'fullname'  => $request->fullname,
+            'state'     => $request->state,
+            'pincode'   => $request->pincode,
+            'city'      => $request->city,
+            'country'   => $request->country,
+            'user_id'   => Auth::id(),
+        ]);
+        dd("done");
 
     }
 
